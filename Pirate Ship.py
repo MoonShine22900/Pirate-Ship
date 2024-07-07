b064b6689bee109d0d48fe950f92339de1e5dd33
MoonShine22900 <leoxdnpc2@gmail.com>
2024-07-07 16:47:14 -0600

    Beta Pirate Ship 1.0.1.py

--- Pirate Ship.py
+++ Pirate Ship.py
@@ -83,6 +83,8 @@ while not bucle:
         print("- Install Homebrew")
         print("- Disable App Store Updates")
         print("- Restart Software Update Service")
+        print("piracy")
+        print("web : shows you a list of craks websites (rebember than this is illegal and is only for testing purposes)" )
         print("TO RUN A COMMAND JUST COPY AND PASTE")
         print("Wanna Undo A command? just tipe default and the code, example default Disable Security MacOS Updates, Install homebrew, Open damaged Programs and Restart Software Update Service dont have this function")
      
@@ -142,5 +144,10 @@ while not bucle:
     elif action == "default INFO":
         print("Cannot use default in this command")
     
+    elif action == "piracy":
+        print("We want to remind you that using and installing pirated software is illegal. We strongly discourage any form of piracy. If you find software valuable, please support its creators by purchasing a legitimate copy. Piracy not only hurts developers but also compromises your system's security. However, if you still decide to use pirated software, consider making donations to the developers or contributing in other ways. Remember, while piracy is illegal and wrong, it is especially unethical if you are generating profits from the pirated software or if you have the means to pay for it. The primary purpose of this program is to allow you to download and try software that might have limited trial versions. If you find the software useful, please purchase it to support the developers. If you do not like it, please delete it. Supporting legal software ensures continued development and innovation. If you are unable to afford the software, consider looking for free or open-source alternatives. Let’s support the hardworking developers who bring valuable tools and applications to the world. Please note that I am not responsible if you encounter viruses or other security issues as a result of using pirated software.")
+    
+    elif action == "web":
+        print(GREEN + "https://www.artistapirata.com", BLUE + "https://www.audiotools.in", GREEN + "https://thepiratebay.org/index.html" + "to open just press command and then press the link you want to open, REBEMBER, JUST TEST IT, DONT HAVE IT MORE THAN 1 WEEK")
     else:
         print("Invalid command. Please try again.")
